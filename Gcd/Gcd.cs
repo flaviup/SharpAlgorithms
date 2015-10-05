c1d7eb0b4242be87a23c9239dce6860953e76853
Flaviu Pasca <flaviu.pasca@cubictelecom.com>
2015-10-05 23:58:32 +0100

    Added Euclid's greatest common divisor algorithm

---
+++ Gcd/Gcd.cs
@@ -0,0 +1,38 @@
+﻿
+// Flaviu Pasca
+// flaviup @ gmail.com
+// (C) 2015
+
+using System;
+
+namespace SharpAlgorithms
+{
+	public class Gcd
+	{
+		public Gcd (long a, long b)
+		{
+			Value = Compute (a, b);
+		}
+
+		public long Value { get; private set; }
+
+		public static long Compute (long a, long b)
+		{
+			while (b != 0)
+			{
+				Assign (ref a, ref b, b, a % b);
+				// Alternative implementation:
+				/*var t = Tuple.Create (b, a % b);
+				a = t.Item1;
+				b = t.Item2;*/
+			}
+			return a;
+		}
+
+		private static void Assign (ref long a, ref long b, long a2, long b2)
+		{
+			a = a2;
+			b = b2;
+		}
+	}
+}
